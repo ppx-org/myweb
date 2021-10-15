37f37c359553862ef4db92e46e562f289ed71ea1
mark <6882855@qq.com>
2021-10-15 23:25:23 +0800

    ttt

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="" style="background-color: white; height:300px">
-    <el-divider content-position="left">公告</el-divider>
-
+  <div class="" style="background-color: white; ">
+    <el-divider content-position="left" style="padding-top:5px"><h3><i class="el-icon-star-on"></i>快速<i
+        class="el-icon-star-on"></i></h3></el-divider>
     <div style="margin-left:20px">
-      <h3>快速开发</h3>
+      <ul>
+        <li>Eclipse + Jrebel可实现修改代码或SQL立即生效</li>
+        <li>IntelliJ IDEA + vue3 + VITE可修改代码立即生效和快速启动、强大提示</li>
+        <li>java17不再需要拼装SQL</li>
+        <li>良好的框架，减少代码代码量，更加关注业务开发(去if,try...catch,...)</li>
+      </ul>
+    </div>
+    <el-divider content-position="left" style="padding-top:5px"><h3><i class="el-icon-star-on"></i>安全<i
+        class="el-icon-star-on"></i></h3></el-divider>
+    <div style="margin-left:20px">
+      <ul>
+        <li>防sql注入</li>
+        <li>密码安全，加密方式，退出立即生效，修改密码立即生效</li>
+        <li>update或delete不加where不会警告</li>
+        <li>防止CSRF跨站请求伪造</li>
+      </ul>
+    </div>
+    <el-divider content-position="left" style="padding-top:5px"><h3><i class="el-icon-star-on"></i>性能<i
+        class="el-icon-star-on"></i></h3></el-divider>
+    <div style="margin-left:20px">
+      <ul>
+        <li>不会打一堆异常日志</li>
+        <li>没有万能Map查询条件，造成内存溢出</li>
+      </ul>
+    </div>
 
-      <ul><li>eclipse + Jrebel可实现修改代码或SQL立即生效</li><li>idea + vue3 + VITE可修改代码立即生效和快速启动、强大提示</li><li>java17不再需要拼装SQL</li><li>良好的框架，减少代码代码量，更加关注业务开发(去if,try...catch,...)</li><li>安全性
-        &gt; sql注入
-        &gt;
-        &gt; 密码安全，加密方式，退出立即生效，修改密码立即生效
-        &gt;
-        &gt; update或delete不加where不会警告
-        &gt;<br/>&gt; 防止CSRF跨站请求伪造</li></ul>
+    <div>
+      &nbsp;
     </div>
   </div>
 </template>
