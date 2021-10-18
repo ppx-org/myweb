4b0f3b03da3078aa7e93d0c47c17dc52348b80f1
deng <6882855@qq.com>
2021-10-18 11:24:46 +0800

    t

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,35 +2,36 @@
   <div class="" style="background-color: white; ">
     <el-divider content-position="left" style="padding-top:5px">
       <h3>
-      <i class="el-icon-star-on"></i>快速<i class="el-icon-star-on"></i>
+        <i class="el-icon-star-on"></i>快速<i class="el-icon-star-on"></i>
       </h3>
     </el-divider>
     <div style="margin-left:20px">
       <ul>
-        <li>后端快速开发：sEclipse + Jrebel可实现修改代码或SQL立即生效</li>
+        <li>后端快速开发：Eclipse + Jrebel可实现修改代码或SQL立即生效</li>
         <li>前端快速开发：IntelliJ IDEA + vue3 + VITE可修改代码立即生效和快速启动、强大提示</li>
         <li>java17不再需要拼装SQL</li>
-		<li>Spring boot优雅停机</li>
-		<li>日期和时间分开，自动格式化</li>
+        <li>Spring boot优雅停机</li>
+        <li>日期和时间分开，自动格式化</li>
         <li>良好的框架，减少代码代码量，更加关注业务开发(去if,try...catch,...)</li>
-		<li>null处理机制</li>
-		<li>异常处理机制，异常拦截，输出控制</li>
-		<li>模块划分</li>
-		<li>优雅返回机制，专注数据返回</li>
-		<li>优雅的SQL写法</li>
-		
+        <li>null处理机制，json的null不返回，新增和修改的null不处理</li>
+        <li>异常处理机制，异常拦截，输出控制</li>
+        <li>模块划分</li>
+        <li>优雅返回机制，专注数据返回</li>
+        <li>优雅的SQL写法</li>
       </ul>
     </div>
     <el-divider content-position="left" style="padding-top:5px"><h3><i class="el-icon-star-on"></i>安全<i
         class="el-icon-star-on"></i></h3></el-divider>
     <div style="margin-left:20px">
       <ul>
-		<li>高效安全方便拦截URI权限方式，使用BitSet和本地缓存</li>
+        <li>高效安全方便拦截URI权限方式，使用BitSet和本地缓存</li>
         <li>防sql注入</li>
-		<li>密码安全，加密保存</li>
+        <li>密码安全，加密保存</li>
         <li>退出立即生效，修改密码立即生效</li>
-        <li>update和delete必须加上where</li>
+        <li>update和delete限制必须加上where</li>
         <li>防止CSRF跨站请求伪造, Cookie安全(去掉)</li>
+        <li>限流针对账号 访问过多</li>
+        <li>get post(查看和操作)分开，允许只能查看权限，可限制数据更新操作次数</li>
       </ul>
     </div>
     <el-divider content-position="left" style="padding-top:5px">
@@ -48,27 +49,25 @@
       </ul>
     </div>
 
-	<el-divider content-position="left" style="padding-top:5px">
+    <el-divider content-position="left" style="padding-top:5px">
       <h3>
         <i class="el-icon-star-on"></i>可扩展性<i class="el-icon-star-on"></i>
-    </h3>
+      </h3>
     </el-divider>
-	<div style="margin-left:20px">
+    <div style="margin-left:20px">
       <ul>
         <li>支持多机轮询调用后端接口</li>
       </ul>
     </div>
-	
-	<div>
-      TODO LIST
-      * 限流一个账号 访问过多
-      * post get分开，限制数据更新操作次数
-      * 限流操作，只能查看
-	  
-    </div>
+
 
     <div>
-      &nbsp;
+      常见的问题:未关闭连接
+
+
+
     </div>
   </div>
+
 </template>
+
