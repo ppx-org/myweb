7b2dbc4193adb33c86821e743f5561ca5181b65a
mark <6882855@qq.com>
2021-10-22 21:31:44 +0800

    ttt

--- src/components/test/-sub-router.js
+++ src/components/test/-sub-router.js
@@ -3,7 +3,7 @@ const MODULE = '/test/'
 
 let subRouter = [
     {path: `${MODULE}/Example`, component: () => import('./Example.vue')},
-
+    {path: `${MODULE}/Example2`, component: () => import('./Example2.vue')},
 ]
 
 export {subRouter};
